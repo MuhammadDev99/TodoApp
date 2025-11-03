76ea3d205af5b41e5aa2d371b9760fe7ef3fa540
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2025-11-03 22:19:17 +0300

    Improved

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import "./Footer.css"
 function Footer() {
     return (
         <div className="footer">
-            <p>Made by Muhammad Al-Khlaf</p>
+            <p>Made by Muhammad Al-Khalaf</p>
         </div>
     )
 }
